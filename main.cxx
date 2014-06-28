5eed524c6f8da049ca72515f4e2ab38882772434
Chris Langford <chris@hellocld.com>
2014-06-28 10:14:00 -0400

    Filled in main
    Should initialize SDL and initialize/update/shutdown the systems
    just fine.
    
    STILL NEED TO SHUT DOWN SDL.

--- main.cxx
+++ main.cxx
@@ -5,12 +5,13 @@
 // Description:		A simple program to test SDL2 with cldECS
 //
 
+#include "../CLD-ECS/world.h"
+#include "Components/utility.h"
+#include "Systems/factory.h"
+#include "Systems/render.h"
 #include <SDL2/SDL.h>
 
 int main (int argc, char[] argv) {
-	//just quick-pasted this from the Factory; that shouldn't be
-	//initializing SDL; keeping it here makes it easier to quit when things
-	//don't even start
 	//initialize SDL2
 	if(SDL_Init(SDL_INIT_EVERYTHING) != 0) {
 		//SDL_Init failed, so spit out an error to the terminal and set
@@ -19,4 +20,28 @@ int main (int argc, char[] argv) {
 		std::cerr<<"SDL_Init ERROR: "<<SDL_GetError()<<std::endl;
 		return 1;
 	}
+
+	//if all is well, create a new World
+	CLD_ECS::World sdltest;
+
+	//create a Utility entity
+	int entityUtility = sdltest.createEntity();
+	sdltest.addComponent(sdltest, new SDL_Testing::Utility());
+
+	//add the factory and render systems
+	sdltest.addSystem(new SDL_Testing::Factory());
+	sdltest.addSystem(new SDL_Testing::Render());
+
+	//initialize the systems
+	sdltest.systemsInit();
+
+	//if nothing is wrong, update the systems
+	while(sdltest.getComponent<SDL_Testing::Utility>(entityUtility)->programState != SDL_Testing::Utility::ABORT) {
+		sdltest.systemsUpdate();
+	}
+
+	//all done with updating, so shutdown
+	sdltest.systemsShutdown();
+
+	return 0;
 }
