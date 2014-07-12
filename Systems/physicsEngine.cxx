ffaec4959559c53c6dbbaa7d36f4ffe3885db16c
Chris Langford <chris@hellocld.com>
2014-07-12 08:58:01 -0400

    Lots of changes to get this working
    There was a lot of simple typo fixing, followed by a correction to
    how I was using pointers/references to access the components of the
    BALL objects. Also added some cerr for debug.

--- Systems/physicsEngine.cxx
+++ Systems/physicsEngine.cxx
@@ -7,12 +7,15 @@
 //
 
 #include "physicsEngine.h"
+#include "../Components/utility.h"
 #include "../Components/box.h"
 #include "../Components/physics.h"
 #include "../CLD-Util/Objects/vector.h"
 #include "../CLD-Util/Objects/box.h"
 #include "../CLD-Util/Collision/collision-aabb.h"
+#include "../CLD-Util/Tools/boxTools.h"
 #include <vector>
+#include <SDL2/SDL.h>
 
 //-----------------------------------------------------------------------------
 // Initialization
@@ -44,66 +47,87 @@ void SDL_Testing::PhysicsEngine::init() {
 
 void SDL_Testing::PhysicsEngine::update() {
 	//one by one, go through entitiesPhysics
-	for(std::vector<int>::iterator i = entitiesPhysics::begin(); i < entitiesPhysics.end(); ++i) {
+	for(std::vector<int>::iterator i = entitiesPhysics.begin(); i < entitiesPhysics.end(); ++i) {
 		//check the physics type
 		switch (library->getComponent<SDL_Testing::Physics>(*i)->type) {
-			case BALL:
+			case SDL_Testing::Physics::Type::BALL:
+				{
 				//create a reference to the ball Box and Physics components
 				//this should DRASTICALLY reduce the number of calls to library functions searching for Components
-				SDL_Testing::Box* ballBox = &(library->getComponent<SDL_Testing::Box>(*i));
-				SDL_Testing::Physics* ballPhysics = &(library->getComponent<SDL_Testing::Physics>(*i));
+				std::cerr<<"...Acquiring Box and Physics components...\n";
+				SDL_Testing::Box& ballBox = *(library->getComponent<SDL_Testing::Box>(*i));
+				SDL_Testing::Physics& ballPhysics = *(library->getComponent<SDL_Testing::Physics>(*i));
+				//set oldTime to currentTime
+				oldTime = currentTime;
+				//get the currentTime
+				currentTime = SDL_GetTicks();
 				//create a "counter" for movement occuring during this frame
-				float moveTime = 1;
+				float moveTime = currentTime - oldTime;
 				//collect all WALL objects
+				std::cerr<<"...Acquiring Walls...\n";
 				std::vector<int> walls;
-				for(std::vector<int>::iterator w = entitiesPhysics::begin(); w < entitiesPhysics.end(); ++w) {
-					if(library->getComponent<SDL_Testing::Physics>(*w)->type == WALL)
+				for(std::vector<int>::iterator w = entitiesPhysics.begin(); w < entitiesPhysics.end(); ++w) {
+					if(library->getComponent<SDL_Testing::Physics>(*w)->type == SDL_Testing::Physics::Type::WALL)
 						walls.push_back(*w);
 				}
 				//start a loop that checks for collisions so long as moveTime != 0
 				while(moveTime > 0) {
+					std::cerr<<"...Checking Collisions...\n";
 					//iterate through each of the walls, checking for a collision between it and the BALL
-					//store the ID of the WALL
-					int nearestCollidingWall = wall[0];
-					//store the time of collision (start at 1, which is no collision)
-					float collisionTime = 1;
-					//store the normals of the potential collision
-					CLD_Util::Objects::vec2d normals;
-					//loop through the walls
-					for(std::vector<int>::iterator walls = wall::begin(); walls < wall::end(); ++walls) {
-						//check for a collision; if one occurs in a shorter timespan than collisionTime currently stores, store the new time and the wall's ID
-						float checkTime = CLD_Util::Collision_AABB::aabbSweepCheck(ballBox->box,
-								ballPhysics->velocity * moveTime,
-								library->getComponent<SDL_Testing::Box>(*walls)->box,
-								normals);
-						if(checkTime < collisionTime) {
-							collisionTime = checkTime;
-							nearestCollidingWall = (*walls);
+					if(walls.size() > 0) {
+						std::cerr<<"...Assigning Initial nearestCollidingWall...\n";
+						//store the ID of the WALL
+						int nearestCollidingWall = walls[0];
+						//store the time of collision (start at 1, which is no collision)
+						std::cerr<<"...Assigning Initial collisionTime...\n";
+						float collisionTime = 1;
+						//store the normals of the potential collision
+						std::cerr<<"...Creating Initial normal...\n";
+						CLD_Util::Objects::vec2d normal = CLD_Util::Objects::vec2d(0, 0);
+						//loop through the walls
+						for(std::vector<int>::iterator wallCheck = walls.begin(); wallCheck < walls.end(); ++wallCheck) {
+							//check for a collision; if one occurs in a shorter timespan than collisionTime currently stores, store the new time and the wall's ID
+							std::cerr<<"...Getting checkTime...\n";
+							float checkTime = CLD_Util::Collision_AABB::aabbSweepCheck(ballBox.box,
+									ballPhysics.velocity * moveTime,
+									library->getComponent<SDL_Testing::Box>(*wallCheck)->box,
+									normal);
+							if(checkTime < collisionTime) {
+								std::cerr<<"...Assigning checkTime and Wall...\n";
+								collisionTime = checkTime;
+								nearestCollidingWall = (*wallCheck);
+							}
 						}
-					}
-					//if collisionTime == 1 (no collision), move the ball the remainder of moveTime
-					if(collisionTime == 1) {
-						CLD_Util::Tools::boxMove(ballBox->box, ballPhysics->velocity * moveTime);
-						//set moveTime to 0, so the loop ends
-						moveTime = 0;
+						//if collisionTime == 1 (no collision), move the ball the remainder of moveTime
+						if(collisionTime == 1) {
+							CLD_Util::Tools::boxMove(ballBox.box, ballPhysics.velocity * moveTime);
+							//set moveTime to 0, so the loop ends
+							moveTime = 0;
+						} else {
+							//now that we have the nearest collision and collisionTime, we move the ball to the collisionTime, adjust the velocity based on the collision, and subtract collisionTime from moveTime
+							CLD_Util::Tools::boxMove(ballBox.box, ballPhysics.velocity * collisionTime);
+							moveTime -= collisionTime;
+							//adjust the velocity based on the collision normal
+							if((normal.x > 0 && ballPhysics.velocity.x < 0)||(normal.x < 0 && ballPhysics.velocity.x > 0))
+								ballPhysics.velocity.x *= -1;
+							if((normal.y > 0 && ballPhysics.velocity.y < 0)||(normal.y < 0 && ballPhysics.velocity.y > 0))
+								ballPhysics.velocity.y *= -1;
+
+						}
+						//this will repeat over and over until all collisions have been accounted for
 					} else {
-						//now that we have the nearest collision and collisionTime, we move the ball to the collisionTime, adjust the velocity based on the collision, and subtract collisionTime from moveTime
-						CLD_Util::Tools::boxMove(ballBox->box, ballPhysics->velocity * collisionTime);
-						moveTime -= collisionTime;
-						//adjust the velocity based on the collision normal
-						if((normal.x > 0 && ballPhysics->velocity.x < 0)||(normal.x < 0 && ballPhysics->velocity.x > 0))
-							ballPhysics->velocity.x *= -1;
-						if((normal.y > 0 && ballPhysics->velocity.y < 0)||(normal.y < 0 && ballPhysics->velocity.y > 0))
-							ballPhysics->velocity.y *= -1;
-
+						//there aren't any walls, so
+						//move the ball
+						CLD_Util::Tools::boxMove(ballBox.box, ballPhysics.velocity * moveTime);
+						moveTime = 0;
 					}
-
-					//this will repeat over and over until all collisions have been accounted for
 				}
-			break;
-			case PARTICLE:
+				break;
+				}
+			case SDL_Testing::Physics::Type::PARTICLE:
 				//just move it, since particles don't react to
 				//physics stuff in this demo
+				break;
 
 		}
 	}
@@ -113,6 +137,6 @@ void SDL_Testing::PhysicsEngine::update() {
 // Shutdown
 //-----------------------------------------------------------------------------
 
-void SDL_Testing::PhysicsSystem::shutdown() {
+void SDL_Testing::PhysicsEngine::shutdown() {
 	//nada
 }
