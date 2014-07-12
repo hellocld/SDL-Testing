0a8b6e32dcd22af1061f82d1fd44faf690b84999
Chris Langford <chris@hellocld.com>
2014-07-12 08:52:55 -0400

    Added initial velocity and object TYPE
    Initial velocity will now always be (0, 0), and you can tell the
    object to be a BALL, WALL, or PARTICLE.

--- Components/physics.h
+++ Components/physics.h
@@ -9,13 +9,16 @@
 #ifndef PHYSICS_H
 #define PHYSICS_H
 
-#include "..CLD-ECS/component.h"
+#include "../CLD-ECS/component.h"
+#include "../CLD-Util/Objects/vector.h"
 
 namespace SDL_Testing {
 	class Physics : public CLD_ECS::Component {
 		public:
-			CLD_Util::Objects::vec2d velocity;
-	}
+			CLD_Util::Objects::vec2d velocity = CLD_Util::Objects::vec2d(0, 0);
+			enum Type { WALL, BALL, PARTICLE };
+			Type type;
+	};
 }
 
 #endif
